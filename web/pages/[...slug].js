90fb88508009beea0dafb6b0013beda25849ecbc
Scott Wambach <scottcwambach@gmail.com>
2020-12-22 18:09:25 -0600

    chore: Live Preview for Page builder pages

--- web/pages/[...slug].js
+++ web/pages/[...slug].js
@@ -1,46 +1,68 @@
-import sanityClient from '../client';
 import Layout from '../components/Layout';
 import PageContent from '../components/docTypes/PageContent';
+import { useRouter } from 'next/router';
 import { groq } from 'next-sanity';
 import PostContent from '../components/docTypes/PostContent';
+import { getClient, usePreviewSubscription } from '../utils/sanity';
 
 const query = groq`*[slug.current == $slug][0]{
   "content": *[slug.current == $slug][0],
-  "references": *[references(^._id)],
-  "site": {
-    "events": *[_type == "event"],
-    "settings":  *[_type == "siteSettings"][0],
-    "menus": *[_type == "menu"],
-    "placeholders": *[_type == "sanity.imageAsset"] {
-      "_id": _id,
-      "lqip": metadata.lqip,
-      "palette": metadata.palette,
-      "dimensions": metadata.dimensions
-    }
-  }
 }`;
 
-const PageBuilder = (props) => {
+export default function PageBuilder({ doc, preview }) {
+  const router = useRouter();
+  if (!router.isFallback && !doc) {
+    return <Error statusCode={404} />;
+  }
+
+  const { data = {} } = usePreviewSubscription(query, {
+    params: { slug: doc.content.slug.current },
+    initialData: doc,
+    enabled: router.query.preview === '',
+  });
+
   return (
-    <Layout page={props.content} site={props.site}>
-      {props.content ? (
-        props.content._type === 'page' ? (
-          <PageContent {...props.content} />
+    <Layout page={data.content} site={doc.site}>
+      {data ? (
+        data.content._type === 'page' ? (
+          <PageContent {...data.content} />
         ) : (
-          <PostContent {...props.content} />
+          <PostContent {...data.content} />
         )
       ) : (
         '404 Page Not Found'
       )}
     </Layout>
   );
-};
+}
 
-export async function getServerSideProps(context) {
-  const { slug = '' } = context.query;
-  const content = await sanityClient.fetch(query, {
-    slug: `${slug.join('/')}`,
-  });
+export async function getServerSideProps({
+  query,
+  params = {},
+  preview = false,
+}) {
+  const { slug = '' } = params;
+
+  const doc = await getClient(query.preview === '').fetch(
+    `*[slug.current == $slug][0]{
+    "content": *[slug.current == $slug][0],
+    "references": *[references(^._id)],
+    "site": {
+      "events": *[_type == "event"],
+      "settings":  *[_type == "siteSettings"][0],
+      "menus": *[_type == "menu"],
+      "placeholders": *[_type == "sanity.imageAsset"] {
+        "_id": _id,
+        "lqip": metadata.lqip,
+        "palette": metadata.palette,
+        "dimensions": metadata.dimensions
+      }
+    }
+  }`,
+    {
+      slug: `${slug.join('/')}`,
+    }
+  );
 
   if (slug[0] === 'admin' || slug[0] === 'login' || slug[0] === 'studio') {
     context.res.writeHead(307, {
@@ -49,7 +71,5 @@ export async function getServerSideProps(context) {
     context.res.end();
   }
 
-  return { props: content };
+  return { props: { doc, preview } };
 }
-
-export default PageBuilder;
