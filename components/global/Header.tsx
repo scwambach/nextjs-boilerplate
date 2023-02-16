a13e859c6cdd95f03756e67c705a5c07a6651608
Scott Wambach <scott@scottwamba.ch>
2023-02-16 15:56:32 -0600

    stripped to the basics

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,31 +1,5 @@
-import Image from 'next/image'
-
 const Header = () => {
-  return (
-    <header className="description">
-      <p>
-        Get started by editing&nbsp;
-        <code className="code">pages/index.tsx</code>
-      </p>
-      <div>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By{' '}
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className="vercelLogo"
-            width={100}
-            height={24}
-            priority
-          />
-        </a>
-      </div>
-    </header>
-  )
+  return <header>HEADER</header>
 }
 
 export { Header }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,5 @@
-import Image from 'next/image'
-
 import { Layout } from '@components/wrappers/Layout'
 
 export default function Home() {
-  return (
-    <Layout>
-      <div className="center">
-        <Image
-          className="logo"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className="thirteen">
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-    </Layout>
-  )
+  return <Layout>INDEX</Layout>
 }
