2e4eb3fc3cc9396ffd95e8dc744ead3dfee04afe
Scott Wambach <scott@scottwamba.ch>
2023-02-21 17:00:29 -0600

    responsiveImage

--- components/modules/ResponsiveImage.tsx
+++ components/modules/ResponsiveImage.tsx
@@ -1,42 +1,37 @@
-import { useIntersectionObserver } from '@utils/useIntersectionObserver'
-import React, { useRef, useState, useEffect } from 'react'
-
-const lqip =
-  'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"%3E%3Cfilter id="a" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"%3E%3CfeGaussianBlur stdDeviation="2" result="b"%3E%3C/feGaussianBlur%3E%3CfeComponentTransfer%3E%3CfeFuncA type="discrete" tableValues="0 1"%3E%3C/feFuncA%3E%3C/feComponentTransfer%3E%3C/filter%3E%3Crect filter="url(%23a)" width="20" height="20" fill="%23fff" /%3E%3C/svg%3E'
+import Image from 'next/image'
 
 interface ImageProps {
-  src: string
   alt: string
-  sizes?: string
-  width?: number
   height?: number
+  isBackground?: boolean
+  lqip?: string
+  sizes?: string
+  src: string
+  width?: number
 }
 
-const ResponsiveImage = ({ src, alt, sizes, width, height }: ImageProps) => {
-  const [fullSrc, setFullSrc] = useState('')
-  const ref = useRef<HTMLImageElement>(null)
-  const isIntersecting = useIntersectionObserver({ ref })
-
-  useEffect(() => {
-    if (isIntersecting) {
-      setFullSrc(src)
-    }
-  }, [isIntersecting, src])
-
+const ResponsiveImage = ({
+  alt,
+  height,
+  isBackground,
+  lqip,
+  sizes,
+  src,
+  width,
+}: ImageProps) => {
   return (
-    <img
-      ref={ref}
-      src={fullSrc || lqip}
-      srcSet={
-        fullSrc
-          ? `${src}?w=768 768w, ${src}?w=1024 1024w, ${src}?w=1200 1200w`
-          : undefined
-      }
-      sizes={sizes || undefined}
-      width={width}
-      height={height}
-      alt={alt}
-    />
+    <div data-label={`${isBackground ? 'background' : 'image'}-container`}>
+      <Image
+        src={src}
+        alt={alt}
+        fill={isBackground}
+        sizes={sizes}
+        width={!isBackground ? width : undefined}
+        height={!isBackground ? height : undefined}
+        placeholder={lqip ? 'blur' : undefined}
+        blurDataURL={lqip}
+      />
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,24 @@ import { Layout } from '@components/wrappers/Layout'
 export default function Home() {
   return (
     <Layout>
-      <ResponsiveImage
-        src="https://cdn.sanity.io/images/l977smc4/production/b4dac4aef757e8ccb4c00d1184ece230f21d1ca7-3675x2558.jpg"
-        alt="A picture of Rachel"
-        width={3675 / 5}
-        height={2558 / 5}
-      />
       <Form />
+
+      <div
+        style={{
+          height: '400px',
+          position: 'relative',
+        }}
+      >
+        <ResponsiveImage
+          src="https://cdn.sanity.io/images/l977smc4/production/b4dac4aef757e8ccb4c00d1184ece230f21d1ca7-3675x2558.jpg"
+          lqip="https://cdn.sanity.io/images/l977smc4/production/b4dac4aef757e8ccb4c00d1184ece230f21d1ca7-3675x2558.jpg?w=10&blur=10"
+          isBackground
+          alt="A picture of Rachel"
+          width={3675}
+          height={2558}
+        />
+        <div className="test">test</div>
+      </div>
     </Layout>
   )
 }
