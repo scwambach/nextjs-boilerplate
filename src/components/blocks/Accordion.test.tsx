70739139e9374568fbb3afd05b8c921e63db46a1
Scott Wambach <scott@scottwamba.ch>
2024-01-26 16:02:10 -0600

    testing setup

---
+++ src/components/blocks/Accordion.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { Accordion } from './Accordion'
+
+describe('Accordion component', () => {
+  it('renders with heading', () => {
+    const heading = 'Accordion Heading'
+
+    const { getByText, queryByText } = render(
+      <Accordion heading={heading}>
+        <p>Accordion Content</p>
+      </Accordion>
+    )
+
+    const summaryElement = getByText(heading)
+    expect(summaryElement).toBeInTheDocument()
+
+    // Initially, content should be hidden
+    const contentElement = queryByText('Accordion Content')
+    expect(contentElement).not.toBeVisible()
+
+    // Click on the summary to toggle visibility
+    fireEvent.click(summaryElement)
+
+    // After clicking, content should become visible
+    expect(queryByText('Accordion Content')).toBeInTheDocument()
+
+    // Click again to hide the content
+    fireEvent.click(summaryElement)
+
+    // After clicking again, content should become hidden
+    expect(queryByText('Accordion Content')).not.toBeVisible()
+  })
+})
---
+++ src/components/blocks/Alert.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { Alert } from './Alert'
+import '@testing-library/jest-dom'
+
+describe('Alert component', () => {
+  it('renders alert with correct message and type', () => {
+    const alertId = 'testAlert'
+    const message = 'Test message'
+    const type = 'success'
+
+    const { getByText } = render(
+      <Alert alertId={alertId} message={message} type={type} />
+    )
+
+    const alertElement = getByText(message)
+    expect(alertElement).toBeInTheDocument()
+    expect(alertElement.parentElement).toHaveClass(`aldert ${type}`)
+  })
+
+  it('hides alert on close button click', () => {
+    const alertId = 'testAlert'
+    const message = 'Test message'
+    const type = 'success'
+
+    const { getByTestId } = render(
+      <Alert alertId={alertId} message={message} type={type} />
+    )
+
+    const closeButton = getByTestId('closeButton')
+    fireEvent.click(closeButton)
+
+    expect(sessionStorage.getItem(`alert_${alertId}`)).toBe('true')
+  })
+
+  it('hides alert if alertId is not provided', () => {
+    const message = 'Test message'
+    const type = 'success'
+
+    const { getByText } = render(<Alert message={message} type={type} />)
+
+    const alertElement = getByText(message)
+    expect(alertElement).toBeInTheDocument()
+    expect(alertElement.parentElement).toHaveClass(`alert ${type}`)
+  })
+
+  it('hides alert if sessionStorage contains alertId', () => {
+    const alertId = 'testAlert'
+    const message = 'Test message'
+    const type = 'success'
+
+    sessionStorage.setItem(`alert_${alertId}`, 'true')
+
+    const { getByText } = render(
+      <Alert alertId={alertId} message={message} type={type} />
+    )
+
+    const alertElement = getByText(message)
+    expect(alertElement).toBeInTheDocument()
+    expect(alertElement.parentElement).toHaveClass(`alert ${type}`)
+  })
+})
