a1b070ecbfa8980346cce98fa40e06b424bbad52
Scott Wambach <scott@scottwamba.ch>
2024-02-01 15:43:17 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,7 @@ export default function Home() {
   return (
     <main>
       <div className="container wide">
-        {repeater(
-          3,
-          <Avatar
-            key={Math.random()}
-            size={5}
-            firstName="Scott"
-            lastName="Wambach"
-          />
-        )}
+        {repeater(3, <Avatar size={5} firstName="Scott" lastName="Wambach" />)}
       </div>
     </main>
   )
