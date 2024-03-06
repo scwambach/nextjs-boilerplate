b9a6f80b9785c67b2f61fbc8555466ef025bf8d4
Scott Wambach <scott@scottwamba.ch>
2024-03-06 16:54:49 -0600

    remove material

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,35 +1,11 @@
 import type { Preview } from "@storybook/react";
 import "../src/styles/main.scss";
 import "./storybook.css";
-import { CssBaseline, createTheme, ThemeProvider } from "@material-ui/core";
-import { withThemeFromJSXProvider } from "@storybook/addon-styling";
 import DocumentationTemplate from "./DocumentationTemplate.mdx";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#12263A",
-    },
-    secondary: {
-      main: "#1668B4",
-    },
-  },
-});
-
-export const decorators = [
-  withThemeFromJSXProvider({
-    themes: {
-      main: theme,
-    },
-    defaultTheme: "main",
-    Provider: ThemeProvider,
-    GlobalStyles: CssBaseline,
-  }),
-];
 
 const preview: Preview = {
   parameters: {
-    decorators,
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
