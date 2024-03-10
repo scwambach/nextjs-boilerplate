b54ddc4510c6595d9f4902c4a21466bc712766e3
Scott Wambach <scott@scottwamba.ch>
2024-03-10 12:57:52 -0500

    added contact to story

--- src/stories/global/PageLayout.stories.tsx
+++ src/stories/global/PageLayout.stories.tsx
@@ -1,7 +1,7 @@
 import { Banner } from '@components/blocks'
 import { PageLayout as PageLayoutItem } from '@components/global/PageLayout'
 import type { Meta, StoryObj } from '@storybook/react'
-import { GlobalProps } from '@utils/types'
+import { ContactInfoProps, GlobalProps } from '@utils/types'
 
 const meta: Meta<typeof PageLayoutItem> = {
   title: 'Components/Global',
@@ -19,6 +19,45 @@ export const PageLayout: Story = {
       siteTitle: 'Next.js Starter Boilerplate',
       siteDescription: 'A starter boilerplate for Next.js',
       favicon: '/favicon.ico',
+      contact: {
+        phone: '555-555-5555',
+        email: 'email@email.com',
+        address: {
+          street: '1234 Street',
+          city: 'City',
+          state: 'ST',
+          zip: '12345',
+        },
+        hours: [
+          {
+            days: 'Monday',
+            hours: '9:00am - 5:00pm',
+          },
+          {
+            days: 'Tuesday',
+            hours: '9:00am - 5:00pm',
+          },
+          {
+            days: 'Wednesday',
+            hours: '9:00am - 5:00pm',
+          },
+          {
+            days: 'Thursday',
+            hours: '9:00am - 5:00pm',
+          },
+          {
+            days: 'Friday',
+            hours: '9:00am - 5:00pm',
+          },
+        ],
+        socials: [
+          {
+            icon: 'FacebookLogo',
+            href: 'https://facebook.com',
+            screenReader: 'Facebook',
+          },
+        ],
+      } as ContactInfoProps,
       navigation: [
         {
           label: 'Home',
