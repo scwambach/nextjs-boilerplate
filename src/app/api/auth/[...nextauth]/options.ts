4efc75bbe5bf3cc9f9542a5c9fcaa076e5902d9a
Scott Wambach <scott@scottwamba.ch>
2024-03-12 11:43:31 -0500

    env vars

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,8 +3,8 @@ import GitHubProvider from 'next-auth/providers/github'
 export const authOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.NEXTAUTH_GITHUB_ID as string,
+      clientSecret: process.env.NEXTAUTH_GITHUB_SECRET as string,
     }),
   ],
 }
