67bddc780162481e9b18169973668a257a5b481c
Scott Wambach <scott@scottwamba.ch>
2024-03-12 11:36:34 -0500

    route update

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,10 @@
+import GitHubProvider from 'next-auth/providers/github'
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
+import { authOptions } from './options';
 
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-}
-
-export const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
