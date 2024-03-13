440d8e582a34dddde8ebb7711f57ccd4f9070b88
Scott Wambach <scott@scottwamba.ch>
2024-03-13 15:57:22 -0400

    google provider (#34)
    * google provider
    
    * Update node.js.yml

--- next.config.js
+++ next.config.js
@@ -31,6 +31,11 @@ const nextConfig = {
         hostname: 'avatars.githubusercontent.com',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+      },
     ],
   }
 }
