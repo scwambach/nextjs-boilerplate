5788cd501a1a92870916c497faeb9cff750c2434
Scott Wambach <scott@scottwamba.ch>
2024-05-14 15:20:42 -0500

    theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default async function RootLayout({
 }) {
   const session = await getServerSession()
   return (
-    <html lang="en">
+    <html lang="en" className="theme-override">
       <body>
         <SessionProvider session={session}>{children}</SessionProvider>
       </body>
